15eb44c5eaeb3ef3ba67184240d2c5ca4a6bad48
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-06-29 21:06:02 +0900

    Create PermutationFullSearch.cpp

---
+++ PermutationFullSearch.cpp
@@ -0,0 +1,8 @@
+vector<int> p[N]; for(int i=0; i<N; ++i) p[i] = i;
+do {
+  for(int i=0; i<N; ++i) {
+    int a=p[i], b=p[(i+1)%N];
+    //continue...
+  }
+  //continue...
+}while(next_permutation(p.begin(), p.end());
