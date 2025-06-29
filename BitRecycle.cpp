8f337d7b9fbad9f940aceb27fd6f94b213fc9fef
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-06-29 21:34:55 +0900

    Update BitRecycle.cpp

--- BitRecycle.cpp
+++ BitRecycle.cpp
@@ -1,1 +1,14 @@
-//???
+//ABC407D
+vector<int> possible_domino{0};
+const int domino_vertical = (1U << W) + 1, domino_horizontal = 3;
+for (int i=0, bit=0; i<H; ++i)
+  for (int j=0; j<W; ++j, ++bit) {
+      vector<int> tmp;
+      for (const auto b : possible_domino) {
+          if (j + 1 < W && !(b & domino_horizontal << bit)) tmp.emplace_back(b | domino_horizontal << bit);
+          if (i + 1 < H && !(b & domino_vertical << bit)) tmp.emplace_back(b | domino_vertical << bit);
+      }
+      ranges::move(tmp, back_inserter(possible_domino));
+  }
+  //continue...
+}
